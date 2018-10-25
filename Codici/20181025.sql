474bc37a581f6bc493530dc36fb863e8cdd8dead
Matteo Franzil <32071488+mfranzil@users.noreply.github.com>
2018-10-25 19:20:36 +0200

    Semicolons

--- Codici/20181025.sql
+++ Codici/20181025.sql
@@ -4,17 +4,17 @@ WHERE E.eid = W.eid AND D.did = W.did AND D.dname = 'Hardware'
 INTERSECT
 SELECT ename, age
 FROM Emp E, Works W, Dept D
-WHERE E.eid = W.eid AND D.did = W.did AND D.dname = 'Software'
+WHERE E.eid = W.eid AND D.did = W.did AND D.dname = 'Software';
 
 SELECT ename, age
 FROM Emp E, Works W1, Works W2, Dept D1, Dept D2
 WHERE (E.eid = W1.eid AND D1.did = W1.did AND D1.dname = 'Software') 
-    AND (E.eid = W2.eid AND D2.did = W2.did AND D2.dname = 'Software')
+    AND (E.eid = W2.eid AND D2.did = W2.did AND D2.dname = 'Software');
 
 SELECT did, COUNT(did)
 FROM Works
 GROUP BY did
-HAVING sum(pct_time)> 20*100
+HAVING sum(pct_time)> 20*100;
 
 SELECT ename
 FROM Emp E
@@ -22,7 +22,7 @@ WHERE salary > (
     SELECT MAX(D.budget)
     FROM Dept D NATURAL JOIN Works W  -- nelle subquery le tabelle esterne sono ancora visibili!
     WHERE E.eid = W.eid
-)
+);
 
 SELECT ename
 FROM Emp E
@@ -30,24 +30,24 @@ WHERE salary > ALL(
     SELECT D.budget
     FROM Dept D NATURAL JOIN Works W 
     WHERE E.eid = W.eid
-)
+);
 
 SELECT D.managerid
 FROM Dept D
 GROUP BY managerid
-HAVING MIN(budget) > 1000000
+HAVING MIN(budget) > 1000000;
 
 SELECT E.ename
 FROM Emp E JOIN Dept D ON managerid = eid
 WHERE budget = (
     SELECT MAX(budget)
     FROM Dept D
-)
+);
 
 SELECT managerid
 FROM Dept D
 GROUP BY managerid
-HAVING SUM(budget) > 5000000
+HAVING SUM(budget) > 5000000;
 
 SELECT managerid
 FROM (
@@ -58,7 +58,7 @@ FROM (
 WHERE cb =  (
     SELECT MAX(cb)
     FROM ControlM
-)
+);
 
 -- Data to create the database:
 
@@ -84,7 +84,7 @@ CREATE TABLE Works(
     FOREIGN KEY(eid) REFERENCES Emp,
     FOREIGN KEY(did) REFERENCES Dept,
     PRIMARY KEY(eid, did)
-)
+);
 
 INSERT INTO Emp VALUES
 (DEFAULT, 'Olivera', 1, 21403.1),
