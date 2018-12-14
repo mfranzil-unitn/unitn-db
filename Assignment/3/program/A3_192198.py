a7f12578eb4e9336bc59a511f6f09737017f4752
Matteo Franzil <32071488+mfranzil@users.noreply.github.com>
2018-12-14 12:30:42 +0100

    Final update

--- Assignment/3/program/A3_192198.py
+++ Assignment/3/program/A3_192198.py
@@ -25,7 +25,7 @@ else:
         return time.clock() * (10 ** 9)
 
 connection = psycopg2.connect("dbname='db_016' user='db_016' host='sci-didattica.unitn.it' password='faustoemagro'")
-
+connection.autocommit = True
 cur = connection.cursor()
 
 # Statement 1
@@ -34,7 +34,6 @@ start = timer()
 cur.execute("DROP TABLE IF EXISTS Car;"
             "DROP TABLE IF EXISTS Person;")
 
-connection.commit()
 end = timer()
 print("Step 1 needs " + str(end - start) + " ns")
 
@@ -51,9 +50,8 @@ cur.execute("CREATE TABLE Person ("
             "targa CHAR(25) PRIMARY KEY,"
             "brand CHAR(50) NOT NULL,"
             "color CHAR(30) NOT NULL,"
-            "owner INTEGER REFERENCES Person(id));")
+            "owner INTEGER REFERENCES Person(id) NOT NULL);")
 
-connection.commit()
 end = timer()
 print("Step 2 needs " + str(end - start) + " ns")
 
@@ -63,9 +61,10 @@ people_insertion = []
 
 # Set per le chiavi e le altezze
 heights = set()
-keys_list = random.sample(range(0, 2147483647), 1000000)
+keys_list = list(range(1000000))
+random.shuffle(keys_list)
 
-for i in range(0, 999999):
+for i in range(999999):
     key = keys_list[i]
 
     # Genero il nome completamente a caso
@@ -85,19 +84,22 @@ for i in range(0, 999999):
     while True:
         height = round(random.uniform(80, 210), 4)
 
-        if height not in heights and height != 185:
+        if height not in heights and abs(185 - height) > 10e-4:
             heights.add(height)
             break
 
     people_insertion.append(str(key) + "\t" + name + "\t" + address + "\t" + str(age) + "\t" + str(height))
-    # print(str(key) + "\t" + name + "\t" + address + "\t" + str(age) + "\t" + str(height))
+    print(str(key) + "\t" + name + "\t" + address + "\t" + str(age) + "\t" + str(height))
     # people_insertion.append(f"{i}\t{name}\t{address}\t{age}\t{i*0.001}")
 
 # Generazione della chiave per la tupla con altezza 185
 key185 = keys_list[999999]
 
 # Inserisco la tupla con valore 185 in altezza
-people_insertion.append(str(key185) + "\tCentottantacinque\tViadeiSolteri97\t1\t185")
+people_insertion.append(str(key185) + "\t"
+                        +  "".join(random.choices(string.ascii_lowercase, k=12)) + "\t"
+                        +  "".join(random.choices(string.ascii_lowercase, k=10)) + "\t"
+                        +  str(random.randrange(18, 100)) + "\t185")
 
 data = StringIO()
 data.write("\n".join(people_insertion))
@@ -107,7 +109,6 @@ data.seek(0)
 cur.copy_from(data, "Person")
 
 data.close()
-connection.commit()
 end = timer()
 print("Step 3 needs " + str(end - start) + " ns")
 
@@ -116,12 +117,20 @@ start = timer()
 
 car_insertion = []
 
-# Genero una lista di targhe (possibilmente finché non arrivo a 1 milione)
-plates = list(set("".join(random.choices(string.ascii_uppercase + string.digits, k=10)) for _ in range(0, 1000000)))
-while len(plates) != 1000000:
-    plates = list(set("".join(random.choices(string.ascii_uppercase + string.digits, k=10)) for _ in range(0, 1000000)))
+def targa(item, seq):
+    item *= 26
+    d0 = item // (26 ** 4)
+    d1 = (item % 26 ** 4) // 26 ** 3
+    d2 = (item % 26 ** 3) // 26 ** 2
+    d3 = (item % 26 ** 2) // 26
+    return chr(ord('A') + d0) + chr(ord('A') + d1) + str(seq).zfill(3) + chr(ord('A') + d2) + chr(ord('A') + d3)
+
+plates = list({targa(count // 26, count % 26) for count in range(1000000)})
 
 random.shuffle(keys_list)
+random.shuffle(plates)
+
+assert(len(plates) == 1000000)
 
 for i in range(0, 1000000):
     targa = plates[i]
@@ -139,7 +148,7 @@ for i in range(0, 1000000):
 
     car_insertion.append(targa + "\t" + brand + "\t" + color + "\t" + str(fk))
     # car_insertion.append(f"{targa}\t{brand}\t{color}\t{i}")
-    # print(f"{targa}\t{brand}\t{color}\t{fk}")
+    print(f"{targa}\t{brand}\t{color}\t{fk}")
 
 data = StringIO()
 data.write("\n".join(car_insertion))
@@ -147,7 +156,6 @@ data.seek(0)
 
 cur.copy_from(data, "Car")
 
-connection.commit()
 end = timer()
 print("Step 4 needs " + str(end - start) + " ns")
 
@@ -160,7 +168,6 @@ data = cur.fetchall()
 for item in data:
     print(item[0], file=sys.stderr)
 
-connection.commit()
 end = timer()
 sys.stderr.flush()
 print("Step 5 needs " + str(end - start) + " ns")
@@ -170,20 +177,18 @@ start = timer()
 
 cur.execute("UPDATE Person SET height = 200 WHERE height = 185")
 
-connection.commit()
 end = timer()
 print("Step 6 needs " + str(end - start) + " ns")
 
 # Statement 7
 start = timer()
 
-cur.execute("SELECT * FROM Person WHERE height = 200")
+cur.execute("SELECT id, address FROM Person WHERE height = 200")
 data = cur.fetchall()
 
 for item in data:
     print(str(item[0]) + "," + str(item[1]), file=sys.stderr)
 
-connection.commit()
 end = timer()
 sys.stderr.flush()
 print("Step 7 needs " + str(end - start) + " ns")
@@ -191,9 +196,8 @@ print("Step 7 needs " + str(end - start) + " ns")
 # Statement 8
 start = timer()
 
-cur.execute("CREATE INDEX height_idx ON Person (height)")
+cur.execute("CREATE INDEX height_idx ON Person USING btree (height)")
 
-connection.commit()
 end = timer()
 print("Step 8 needs " + str(end - start) + " ns")
 
@@ -206,7 +210,6 @@ data = cur.fetchall()
 for item in data:
     print(item[0], file=sys.stderr)
 
-connection.commit()
 end = timer()
 sys.stderr.flush()
 print("Step 9 needs " + str(end - start) + " ns")
@@ -216,20 +219,18 @@ start = timer()
 
 cur.execute("UPDATE Person SET height = 210 WHERE height = 200")
 
-connection.commit()
 end = timer()
 print("Step 10 needs " + str(end - start) + " ns")
 
 # Statement 11
 start = timer()
 
-cur.execute("SELECT * FROM Person WHERE height = 210")
+cur.execute("SELECT id, address FROM Person WHERE height = 210")
 data = cur.fetchall()
 
 for item in data:
     print(str(item[0]) + "," + str(item[1]), file=sys.stderr)
 
-connection.commit()
 end = timer()
 sys.stderr.flush()
 print("Step 11 needs " + str(end - start) + " ns")
